524ea62980748bbc7df741502597c38a02213cb8
Mojtaba Alkhalifah <mojtaba989@gmail.com>
2021-04-13 14:26:18 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# RPi Flask NeoPixel
+    Control an led strip through a web interface.
+## Description
+    This project is a flask web app that runs on a raspberry pi and conterols an led strip.
---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+This file should be the entry pint for the project
+
+This file was written by and is a property of Mujtaba Alkhalifah, a humble Software Engineering graduate form King Fahd University of Petroleum and Minerals
+
+"""
+
+
+def main():
+    print("Hello!!")
+
+
+if __name__ == "__main__":
+    main()
