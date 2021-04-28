7444646ccdd53130ec54985a88cf82eb34528696
Mojtaba Alkhalifah <mojtaba989@gmail.com>
2021-04-28 07:29:41 +0300

    Added database controller: - added module db that houses database initialization and fetching features.

---
+++ flaskr/schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS user_type;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE user_type (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    type INTEGER DEFAULT 1,
+    FOREIGN KEY (type) REFERENCES user_type (id)
+);
+
+
+-- ALTER TABLE user
+-- ADD FOREIGN KEY (id) REFERENCES user_type(id);
+
+CREATE TABLE post (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+INSERT INTO user_type
+    (title)
+VALUES
+    ('USER'),
+    ('ADMIN');
